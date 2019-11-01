12c808b949b4101cfbe51cdd93f3a897bcc456bb
Ron Balthazor <rlbaltha@uga.edu>
2019-11-01 16:09:27 -0400

    composer update

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-    PRayno\CasAuthBundle\PRaynoCasAuthBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
