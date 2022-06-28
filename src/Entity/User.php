2e1da212f0ebf176b3655552664a3a81b9af5dd7
Ron Balthazor <ron.balthazor@gmail.com>
2022-06-28 12:59:56 -0400

    user table name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
+ * @ORM\Table(name="eng_user")
  */
 class User implements UserInterface
 {
